13a928782ec48e3eed544c39ef5fc39c5bbfc0d1
Mateus Andreatta <mateus-henrique2001@hotmail.com>
2021-10-28 22:21:43 -0300

    Adicionado classes do SpringSecurity

--- src/main/java/com/example/pontoquente/model/City.java
+++ src/main/java/com/example/pontoquente/model/City.java
@@ -3,10 +3,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @Entity
 @NoArgsConstructor
